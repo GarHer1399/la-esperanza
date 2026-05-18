6485f0e9eb959f513e6e103439939aa4cc4f7558
maxair180 <christianegf93@hotmail.com>
2026-05-17 22:36:39 -0600

    feat(frontend): agregue login y paginas de dashboard

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import api from "../api/axios";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [mensaje, setMensaje] = useState("");
+
+  const iniciarSesion = async (e) => {
+    e.preventDefault();
+
+    try {
+      const respuesta = await api.post("/auth/login", {
+        username,
+        password,
+      });
+
+      localStorage.setItem("token", respuesta.data.token);
+      localStorage.setItem("usuario", JSON.stringify(respuesta.data.usuario));
+
+      setMensaje("Login exitoso");
+
+      window.location.href = "/dashboard";
+    } catch (error) {
+      setMensaje("Usuario o contraseña incorrectos");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-card" onSubmit={iniciarSesion}>
+        <h1>Sistema La Esperanza</h1>
+        <p>Ingreso al sistema agrícola</p>
+
+        <input
+          type="text"
+          placeholder="Usuario"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Iniciar sesión</button>
+
+        {mensaje && <span>{mensaje}</span>}
+      </form>
+    </div>
+  );
+}
+
+export default Login;
